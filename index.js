de1915c22db74f15e794daf114a90a2e94af716d
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-11-22 18:28:04 +0500

    business logic implemented

---
+++ index.js
@@ -0,0 +1,33 @@
+testCase1 = ["OR", ["<", "a", "b"],
+    ["AND", ["==", "c", "d"],
+        ["!=", "e", "f"]
+    ]
+];
+
+testCase2 = ["OR", ["<", ["+", "w", "z"], "b"],
+    ["AND", ["!==", "a", "b"],
+        ["+", "f", "m"]
+    ]
+];
+
+const applyBrackets = (inputArray) => {
+    return {
+        operandOneBrackets: (typeof inputArray[1][1] === "object" || typeof inputArray[1][2] === "object") || (typeof inputArray[1] === "object" && typeof inputArray[2] !== "object"),
+        operandTwoBrackets: (typeof inputArray[2][1] === "object" || typeof inputArray[2][2] === "object") || (typeof inputArray[1] !== "object" && typeof inputArray[2] === "object")
+    }
+}
+
+const operatorProblem = (inputArray) => {
+    if (typeof inputArray === "string")
+        return inputArray
+
+    const {
+        operandOneBrackets,
+        operandTwoBrackets
+    } = applyBrackets(inputArray)
+
+    return `${operandOneBrackets ? `(${operatorProblem(inputArray[1])})` : operatorProblem(inputArray[1])} ${operatorProblem(inputArray[0])} ${operandTwoBrackets ? `(${operatorProblem(inputArray[2])})` : operatorProblem(inputArray[2])}`
+}
+
+console.log(operatorProblem(testCase1))
+console.log(operatorProblem(testCase2))

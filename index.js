03e224c281e2c1d3b35edb01fefa9ec6a753537b
bilalashraf1710 <fakhar.mehdi@ocloudsolutions.net>
2023-11-23 11:29:28 +0500

    feedback inculcated and more test cases added

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-testCase1 = ["OR", ["<", "a", "b"],
+const testCase1 = ["OR", ["<", "a", "b"],
     ["AND", ["==", "c", "d"],
         ["!=", "e", "f"]
     ]
 ];
 
-testCase2 = ["OR", ["<", ["+", "w", "z"], "b"],
+const testCase2 = ["OR", ["<", ["+", "w", "z"], "b"],
     ["AND", ["!==", "a", "b"],
         ["+", "f", "m"]
     ]
 ];
 
+const testCase3 = ["OR",
+    ["<",
+        ["!==", "z", "z"], "z"
+    ],
+    ["AND",
+        ["!==", "m", "w"], "m"
+    ]
+];
+
+const testCase4 = ["OR",
+    ["<", "w", "c"],
+    ["AND", "z", "m"]
+];
+
 const applyBrackets = (inputArray) => {
+
+    const hasInnerObject = (operandNo) => typeof inputArray[operandNo][1] === 'object' ||
+        typeof inputArray[operandNo][2] === "object"
+    
+    const hasDifferentType = (operandNo) => typeof inputArray[operandNo] === 'object' &&
+        typeof inputArray[2 / operandNo] !== "object"
+    
     return {
-        operandOneBrackets: (typeof inputArray[1][1] === "object" || typeof inputArray[1][2] === "object") || (typeof inputArray[1] === "object" && typeof inputArray[2] !== "object"),
-        operandTwoBrackets: (typeof inputArray[2][1] === "object" || typeof inputArray[2][2] === "object") || (typeof inputArray[1] !== "object" && typeof inputArray[2] === "object")
+        operandOneBrackets: hasInnerObject(1) || hasDifferentType(1),
+        operandTwoBrackets: hasInnerObject(2) || hasDifferentType(2)
     }
 }
 
 const operatorProblem = (inputArray) => {
-    if (typeof inputArray === "string")
-        return inputArray
-
+    if (typeof inputArray === "string") return inputArray
     const {
         operandOneBrackets,
         operandTwoBrackets
     } = applyBrackets(inputArray)
-
     return `${operandOneBrackets ? `(${operatorProblem(inputArray[1])})` : operatorProblem(inputArray[1])} ${operatorProblem(inputArray[0])} ${operandTwoBrackets ? `(${operatorProblem(inputArray[2])})` : operatorProblem(inputArray[2])}`
 }
 
 console.log(operatorProblem(testCase1))
 console.log(operatorProblem(testCase2))
+console.log(operatorProblem(testCase3))
+console.log(operatorProblem(testCase4))
